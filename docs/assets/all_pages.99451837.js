a21477737db9f976619e27985505ac51c67ed549
Toni Rogel <toni@rogel.me>
2021-11-30 08:20:42 +0000

    Actualiza iconos ibsalut

--- docs/assets/all_pages.99451837.js
+++ docs/assets/all_pages.99451837.js
@@ -1,4 +1,4 @@
-var Oo=Object.defineProperty;var Do=(r,e,t)=>e in r?Oo(r,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):r[e]=t;var vr=(r,e,t)=>(Do(r,typeof e!="symbol"?e+"":e,t),t);import{l as qr}from"./index.02d6581e.js";/**
+var Oo=Object.defineProperty;var Do=(r,e,t)=>e in r?Oo(r,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):r[e]=t;var vr=(r,e,t)=>(Do(r,typeof e!="symbol"?e+"":e,t),t);import{l as qr}from"./index.22a506aa.js";/**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
